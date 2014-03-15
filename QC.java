7b07fc27977579f3925a8010960154c4fd08aa16
Oscar Robinson <oscarrobinson@btinternet.com>
2014-03-15 13:32:37 +0000

    QC now works with file as argument or as input stream :)

--- QC.java
+++ QC.java
@@ -1,12 +1,20 @@
+import java.io.File;
+import java.io.InputStream;
+import java.io.FileInputStream;
 public class QC{
 
 	public static void main(String args[]) throws Exception {
-         	/* init the automatically generated scanner from calc.lex */
-        	Scanner myScanner = new Scanner(System.in);
-    	    /* init the automatically generated parser from calc.cup */
+			Scanner myScanner;
+			if(args.length > 0) {
+            	File file = new File(args[0]);
+            	InputStream is = new FileInputStream(file);
+            	myScanner = new Scanner(is);
+            }
+        	else{
+        		myScanner = new Scanner(System.in);
+        	}
 	        Parser myParser = new Parser(myScanner);
- 	         /* parse the input program from stdin */
- 	         myParser.debug_parse();
+ 	        myParser.debug_parse();
 
 	}
 }
